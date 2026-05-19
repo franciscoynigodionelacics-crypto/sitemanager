d0bb3dcda1dcc141a98abe96e8680a4af238abab
ArjelAaran <arjelaaran.deocampo.cics@ust.edu.ph>
2026-05-19 09:56:44 +0800

    routing

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
-  await app.listen(3000);
-  console.log('api-gateway running on port 3000');
+  await app.listen(4000);
+  console.log('api-gateway running on port 4000');
 }
 bootstrap();
